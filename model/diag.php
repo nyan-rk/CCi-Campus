1bdfb85e11f16abfa3061816a5b984c605119470
DarHashCCI <alss-dipsw20-hda@ccicampus.fr>
2021-02-11 18:47:54 +0100

    Added a function to retrieve the last 3 diagrams seen (if you still have the permission

--- model/diag.php
+++ model/diag.php
@@ -18,4 +18,10 @@
 		));
     }
 
+    function retrieveLastThreeDiagSeen($idUser, $db)
+    {
+        $req=$db->prepare('SELECT v.id_diag, max(date_viewed) FROM view_history as v INNER JOIN diag as d on v.id_diag=d.id_diag INNER JOIN teams as t on t.id_team=d.team_affili INNER JOIN team_affiliation as t2 on t2.id_team=t.id_team INNER JOIN user as u on t2.id_user=u.id_user WHERE u.id_user=:USER GROUP BY v.id_diag ORDER BY max(date_viewed) DESC LIMIT 3');
+		$req->execute(array('USER' => $idUser));
+
+    }
 ?>
