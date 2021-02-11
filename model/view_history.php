67a3c0776e7a792b765e2e27cd6dd46c79506a73
DarHashCCI <alss-dipsw20-hda@ccicampus.fr>
2021-02-11 17:34:25 +0100

    Added a function to add a diag view in the history

--- model/view_history.php
+++ model/view_history.php
@@ -2,7 +2,7 @@
 
     function addView($idDiag, $idUser, $db)
     {
-        $req=$db->prepare('INSERT INTO view_history (id_diag, id_user, date_viewed) VALUES (:DIAG,:USER, NOW());');
+        $req=$db->prepare('INSERT INTO view_history (id_diag, id_user) VALUES (:DIAG,:USER);');
 		$req->execute(array(
 			'DIAG' => $idDiag,
 			'USER' => $idUser
