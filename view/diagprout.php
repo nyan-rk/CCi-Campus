85117c4e89d9ef148bdc6b8a22008aaa8dd0dd38
DarHashCCI <alss-dipsw20-hda@ccicampus.fr>
2021-02-11 16:33:28 +0100

    Va te faire Git

--- view/diagprout.php
+++ view/diagprout.php
@@ -5,6 +5,7 @@
         $resultat = $req->fetch();
     ?>
     <div class="container">
+        <div id="bio">
         <?php
             echo "<h2 id='diagTitle'>".$resultat["name_diag"]."</h2>";
         ?>
@@ -21,8 +22,8 @@
             <div class="col-6 col-md-3 order-2 order-md-3">
                 <button>Bouton</button>
             </div>
-        </div>
-
+        </div></div>
+        <div id="diagcont">
         <div class="row" id="diag">
             <?php
                 displayDiag($_GET['d'],$diag['newtask'],$diag['newstack'],$bdd);
@@ -31,11 +32,10 @@
             <div class='colo colnew' text1="<?php echo $diag['newtask']?>" text2="<?php echo $diag['newstack']?>"><svg id="newStackButton" width='100px' height='100px'><use href='./public/images/icons/plus.svg#plus'></use></svg></div>
             <?php //echo file_get_contents("./public/images/icons/plus - copie.svg"); ?>
         </div>
-
+        </div>
     </div>
 </body>
 <script src="./public/js/jquery-3.5.1.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-<script src="./public/js/jquery.horizontal.scroll.js"></script>
 <script src="./public/js/diag3.js"></script>
 <script src="./public/js/html2canvas.js"></script>
