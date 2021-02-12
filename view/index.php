752f433c898523c28e2bdfb2b3321196680802b8
nyan-rk <alss-dipsw20-rny@ccicampus.fr>
2021-02-12 08:28:43 +0100

    add view folder

---
+++ view/index.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>View login</title>
+</head>
+    <body>
+
+        <form action="controller/login.php" method="POST">
+
+            <label>Login</label>
+
+            <br>
+
+            <input type="text" name="email">
+            <input type="password" name="password">
+
+            <br>
+
+            <input type="submit" value="Envoyer">
+        </form>
+
+    </body>
+</html>
