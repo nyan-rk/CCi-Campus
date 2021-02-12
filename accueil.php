359fe4718966af0fbf3217e08f03e102372ae192
nyan-rk <alss-dipsw20-rny@ccicampus.fr>
2021-02-12 08:06:26 +0100

    add accueil.php

---
+++ accueil.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+</head>
+    <body>
+
+        <h1>Page d'accueil</h1>
+
+        <a href="accueil.php">Accueil</a>
+
+        <br>
+
+        <a href="index.php">Connexion</a>
+    
+        <br>
+
+        <a href="register.php">Inscription</a>
+
+        <br>
+
+        <a href="home.php">Home</a>
+    </body>
+</html>
