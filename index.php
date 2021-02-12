61ec7309e4eed0952f9e31e455dee9491de23dd0
nyan-rk <alss-dipsw20-rny@ccicampus.fr>
2021-02-12 08:33:19 +0100

    add index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+  // On démarre la session AVANT toute chose
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+    include "./controller/initPage.php";
+    initPage();
+?>
