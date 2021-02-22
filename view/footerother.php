8b2b4d04eac8e114fd7909b8b15c0b47787c0279
DarHashCCI <alss-dipsw20-hda@ccicampus.fr>
2021-02-22 22:45:57 +0100

    Forgot to add the alterative head & footer for BS 5.0.0 pages

---
+++ view/footerother.php
@@ -0,0 +1,17 @@
+        <footer>
+                <div class="container">
+                        <div class="row" style="position:relative;justify-content: center;">
+                                <svg id="logo_footer" width='70px' height='70px'><use href='./public/images/logofooter.svg#logo'></use></svg>
+                                <div>
+                                        <?php echo "<div class='row'>".$footer['copyright']."</div>" ?>
+                                        <br>
+                                        <div class="row" style="justify-content: space-between;">
+                                                <?php echo "<a href='legal.php'>".$footer['legal']."</a>
+                                                 <a href='contact.php'>".$footer['contact']."</a>" ?>
+                                        </div>
+                                </div>
+                        </div>
+                </div>
+        </footer>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</html>
