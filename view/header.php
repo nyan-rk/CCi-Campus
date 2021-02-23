c2c01f0f26598c5d92329bb8217b964f824783cc
nyan-rk <alss-dipsw20-rny@ccicampus.fr>
2021-02-23 18:53:08 +0100

    add navbar in header.php

--- view/header.php
+++ view/header.php
@@ -4,4 +4,16 @@
         <use href='../public/images/logo.svg#logo'></use>
     </svg>
 
+    <ul class="nav justify-content-end">
+        <li class="nav-item">
+            <a class="nav-link active" href="../index.php">Accueil</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="">Contact</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="../login.php">Se connecter</a>
+        </li>
+    </ul>
+
 </header>
