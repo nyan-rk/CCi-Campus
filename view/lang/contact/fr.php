32ae35b68853c99499c178ebb342ea66c648ba14
DarHashCCI <alss-dipsw20-hda@ccicampus.fr>
2021-02-23 22:18:51 +0100

    Started working on the contact page

---
+++ view/lang/contact/fr.php
@@ -0,0 +1,11 @@
+<?php
+
+$title = "Cardly - Contact";
+define("CONT", [
+    'title'=> "Sur quel projet travailler ?",
+    'name'=> "Nom",
+    'firstname'=> "Prénom",
+    'subject'=>"Sujet",
+    'message'=>"Message",
+    'send' => "Envoyer"
+]);
