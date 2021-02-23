1dcda18c9ab57bc2c3d38733be233c988635af64
Your Name <you@example.com>
2021-02-23 18:07:04 +0100

    First commit

--- view/index.php
+++ view/index.php
@@ -1,3 +1,248 @@
 <body>
+
+
+    <!--------------- Welcome part --------------->
+    <div class="container">
+      <div class="row" style="height: 800px">
+        <div class="col-sm text-center pt-5 my-auto">
+          <h1> <strong>Bienvenue sur Cardly</strong></h1> 
+          <p class="pt-3">Nous t'invitons à poursuivre ta lecture afin d'en apprendre plus sur notre site web. N'hésite plus et clique sur le bouton ci-dessous !</p>
+          <div class="row justify-content-center pt-3">
+            <a href="#ancre"><button type="button" class="btn btn-dark col-md-6">Découvrir </button></a>
+          </div>
+        </div>
+        <div class="col-sm py-5 my-auto">
+          <img src="img/imageheader.svg" alt="Header image">
+        </div>
+      </div>
+    </div>
+
+
+    <!--------------- Why Cardly part --------------->
+    <div id="ancre" class="text-center py-5"><h1>Pourquoi utiliser Cardly ?</h1> </div>
+
+    <div class=" text-center container pt-5 pb-5">
+      <div id="picto" class="row">
+        <div class="col-lg">
+          <img src="img/picto_management.png" alt="">
+          <h3 class="pt-5">Gestion</h3>
+          <p>Utilisez les tableaux disponibles sur Cardly afin de vous organiser individuellement ou bien avec votre équipe. </p>
+        </div>
+        <div class="col-lg">
+          <img src="img/picto_customization.png" alt="">
+          <h3 class="pt-5">Personnalisation</h3>
+          <p>Personnalisez quelques détails de votre profil afin de rendre votre expérience plus agréable. </p>
+        </div>
+        <div class="col-lg">
+          <img src="img/picto_optimization.png" alt="">
+          <h3 class="pt-5">Optimisation</h3>
+          <p>Gagnez du temps en utilisant les fonctionnalités existantes sur Cardly et optimisez ainsi votre efficacité. </p>
+        </div>
+      </div>
+      <br><br><br><br><br>
+    </div>
+
+
+    <!--------------- Our team part --------------->
+    <div id="border-top" class="text-center pt-5"><h1>Découvre notre équipe </h1> </div>
+    <div class="text-center py-3" style="color: #798da3;"><p>Voici les membres de l'équipe Cardly. Ils ne sont pas nombreux, mais la qualité prime sur la quantité.</p></div>
+
+    <div class=" text-center container py-5">
+      <div id="avatar" class="row">
+        <div class="col-sm">
+          <img src="img/avatar_darius.svg" alt="">
+          <h3 class="pt-5">Darius</h3>
+          <p>L'homme de la situation. </p>
+        </div>
+        <div class="col-sm">
+          <img src="img/avatar_cecile.svg" alt="">
+          <h3 class="pt-5">Cécile</h3>
+          <p>La jeune brebis.</p>
+        </div>
+        <div class="col-sm">
+          <img src="img/avatar_andy.svg" alt="">
+          <h3 class="pt-5">Andy</h3>
+          <p>Le chef de projet.</p>
+          </div>
+      </div>
+    </div>
+
+
+    <!--------------- What other users think part --------------->
+    <div id="border-top" class="text-center pt-5"><h1>Ce que pensent les autres utilisateurs </h1> </div>
+
+    <div class="row px-5 py-5">
+      <div class="col-lg-4 pt-1">
+        <div class="card">
+          <div class="container my-4">
+            <div class="row">
+              <div class="col-sm-4">
+                <img class="card-img-top" src="img/picto_user_comments.svg" alt="Card image cap">
+              </div>
+              <div class="col-sm-8">
+                <div class="card-body">
+                  <h5 class="card-title">Louis Lutz</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">@lolulu</h6>
+                </div>
+              </div>
+              <p class="card-text">J'adore @Cardly. Simple d'utilisation et toujours agréable à utiliser, je l'utilise hebdomadairement. Un grand merci à l'équipe de développement !</p>
+              </div>
+            </div>
+          </div>
+      </div>
+
+      <div class="col-lg-4 pt-1">
+        <div class="card">
+          <div class="container my-4">
+            <div class="row">
+              <div class="col-sm-4">
+                <img class="card-img-top" src="img/picto_user_comments.svg" alt="Card image cap">
+              </div>
+              <div class="col-sm-8">
+                <div class="card-body">
+                  <h5 class="card-title">Mika Lou</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">@mik_lou</h6>
+                </div>
+              </div>
+              <p class="card-text">Pour moi, s'organiser est devenu une réelle partie de plaisir depuis que j'utilise @Cardly. Beaucoup de points positifs, un design agréable, je recommande.</p>
+              </div>
+            </div>
+          </div>
+      </div>
+
+      <div class="col-lg-4 pt-1">
+        <div class="card">
+          <div class="container my-4">
+            <div class="row">
+              <div class="col-sm-4">
+                <img class="card-img-top" src="img/picto_user_comments.svg" alt="Card image cap">
+              </div>
+              <div class="col-sm-8">
+                <div class="card-body">
+                  <h5 class="card-title">Camille Onette </h5>
+                  <h6 class="card-subtitle mb-2 text-muted">@vroum</h6>
+                </div>
+              </div>
+              <p class="card-text">@Cardly merci pour la superbe découverte. Je compte bien continuer à utiliser votre outil, personne ne rivalise. Continuez ainsi 😀😀.</p>
+              </div>
+            </div>
+          </div>
+      </div>
+
+      <div class="col-lg-4 pt-1">
+        <div class="card">
+          <div class="container my-4">
+            <div class="row">
+              <div class="col-sm-4">
+                <img class="card-img-top" src="img/picto_user_comments.svg" alt="Card image cap">
+              </div>
+              <div class="col-sm-8">
+                <div class="card-body">
+                  <h5 class="card-title">Will</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">@sofresh</h6>
+                </div>
+              </div>
+              <p class="card-text">My favorite tool so far - if you want to be more efficient and manage your projects easily, don't hesitate to use @Cardly, you will not be disappointed.</p>
+              </div>
+            </div>
+          </div>
+      </div>
+
+     <div class="col-lg-4 pt-1">
+        <div class="card">
+          <div class="container my-4">
+            <div class="row">
+              <div class="col-sm-4">
+                <img class="card-img-top" src="img/picto_user_comments.svg" alt="Card image cap">
+              </div>
+              <div class="col-sm-8">
+                <div class="card-body">
+                  <h5 class="card-title">Cerise</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">@sakura</h6>
+                </div>
+              </div>
+              <p class="card-text">Impressionnant ! J'adore utiliser @Cardly au quotidien. J'ai rapidement été séduite par sa simplicité et l'ensemble de ses fonctionnalités. 🤓📝</p>
+              </div>
+            </div>
+          </div>
+      </div>
+
+      <div class="col-lg-4 pt-1">
+        <div class="card">
+          <div class="container my-4">
+            <div class="row">
+              <div class="col-sm-4">
+                <img class="card-img-top" src="img/picto_user_comments.svg" alt="Card image cap">
+              </div>
+              <div class="col-sm-8">
+                <div class="card-body">
+                  <h5 class="card-title">Glaïs</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">@tuligla</h6>
+                </div>
+              </div>
+              <p class="card-text">J'ai découvert @Cardly il y a peu et j'ai été agréablement surprise. J'attends avec impatience l'implémentation de nouvelles fonctionnalités ! </p>
+              </div>
+            </div>
+          </div>
+      </div>
+    </div>
+
+
+    <!--------------- Frequently asked questions part --------------->
+    <div id="border-top" class="text-center pt-5 "><h1>Questions récurrentes</h1> </div>
+    <div class="text-center pt-3" style="color: #798da3;"><p>Si tu as encore quelques interrogations, consulte la rubrique ci-dessous. Cela pourra peut-être t'aider !</p> </div>
+
+    <div class="mx-auto py-5 accordion col-8" id="accordionExample">
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="headingOne">
+          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+            Est-ce que je peux travailler en collaboration avec d'autres personnes ?
+          </button>
+        </h2>
+        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+          <div class="accordion-body">
+            Il est bien sûr possible d'ajouter des membres dans une équipe, afin de pouvoir travailler à plusieurs sur un projet. L'option se trouve sur la page Dashboard du site, dans la partie équipe.
+          </div>
+        </div>
+      </div>
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="headingTwo">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+            Les données transmises sur le site sont-elles sécurisées ?
+          </button>
+        </h2>
+        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+          <div class="accordion-body">
+            Absolument ! Dans la mesure où tu es le seul invité sur un tableau, tous les dossiers reliés sont privées, et tu es donc le/la seul(e) à pouvoir les consulter et les utiliser.
+          </div>
+        </div>
+      </div>
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="headingThree">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+            Que faire si je souhaite utiliser mon téléphone pour consulter mes projets en cours ?
+          </button>
+        </h2>
+        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+          <div class="accordion-body">
+            Cardly ayant été conçu de manière responsive, il est tout à fait possible de naviguer sur le site avec un téléphone ou tout autre support disposant d'une taille d'écran réduite. 
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <!--------------- Get to know us part --------------->
+    <div id="border-top" class="text-center pt-5 "><h1>Rejoins notre projet </h1> </div>
+    <div class="text-center pt-3" style="color: #798da3;">
+      <p>Lance-toi et rejoins notre communauté afin de découvrir toutes les fonctionnalités mises à ta disposition. </p>
+    </div>
+
+    <div class="row text-center py-3 col-md-6 mx-auto">
+      <a href="login.php">
+        <button type="button" class="btn btn-dark col-md-6">Connecte toi </button>
+      </a>
+    </div>
+
 </body>
 <script src="./public/js/jquery-3.5.1.js"></script>
