7eeb22dc5b223e211abcc33e9ab505616577bce2
nyan-rk <alss-dipsw20-rny@ccicampus.fr>
2021-02-24 19:54:16 +0100

    update forgot_password done

--- controller/forgot_password.php
+++ controller/forgot_password.php
@@ -1,6 +1,35 @@
 <?php
+
+require '../model/connexion_bdd_ajax.php';
+
+
 require_once('phpmailer/class.phpmailer.php');
 //include("class.smtp.php"); // optional, gets called from within class.phpmailer.php if not already loaded
+
+$result = generatePassword(8);
+
+//generate new password
+function generatePassword($length = 8)
+{
+    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    $count = mb_strlen($chars);
+
+    for ($i = 0, $result = ''; $i < $length; $i++) {
+        $index = rand(0, $count - 1);
+        $result .= mb_substr($chars, $index, 1);
+    }
+
+    return $result;
+}
+
+
+//pass hashed
+$hashedPassword = password_hash($result, PASSWORD_DEFAULT);
+
+$sql = "UPDATE user SET pass_user = ? WHERE email = ?";
+$db = $bdd->prepare($sql);
+$db->execute([$result, htmlspecialchars($_POST['email'])]);
+
 
 $mail = new PHPMailer(true); // the true param means it will throw exceptions on errors, which we need to catch
 
@@ -8,9 +37,16 @@ $mail->IsSMTP(); // telling the class to use SMTP
 
 echo "<pre>";
 
-$htmlMessage = '<body style="margin: 10px;">' . nl2br(htmlspecialchars($_POST['message'])) . '</body>';
-$subject = htmlspecialchars($_POST['subject']);
-$senderName = htmlspecialchars($_POST['name']) . " " . htmlspecialchars($_POST['firstname']);
+$headers = 'Voici votre nouveau Mot de passe' . '<br>'
+    . $result
+    . '<br>'
+    . 'Veuillez vous rendre sur le lien suivant afin de vous connecter à Cardly.fr
+
+Lien : http://cardly/login.php';
+
+$subject = 'Voici mon test d\'envoi';
+
+
 try {
     $mail->SMTPDebug  = 0;                     // enables SMTP debug information (for testing)
     $mail->SMTPAuth   = true;                  // enable SMTP authentication
@@ -19,15 +55,16 @@ try {
     $mail->Port       = 465;                   // set the SMTP port for the GMAIL server
     $mail->Username   = "teamcardly@gmail.com";  // GMAIL username
     $mail->Password   = "ET6Ee99XgjrDAFq$";            // GMAIL password
-    $mail->AddReplyTo(htmlspecialchars($_POST['email']), $senderName);
-    $mail->AddAddress('teamcardly@gmail.com', 'Cardly Team');
-    $mail->SetFrom(htmlspecialchars($_POST['email']), $senderName);
+    $mail->AddReplyTo('teamcardly@gmail.com');
+    $mail->AddAddress(htmlspecialchars($_POST['email']), 'Votre mail');
+    $mail->SetFrom('teamcardly@gmail.com');
     // https://myaccount.google.com/lesssecureapps
     $mail->Subject = 'Cardly - ' . $subject;
     $mail->AltBody = 'To view the message, please use an HTML compatible email viewer!'; // optional - MsgHTML will create an alternate automatically
-    $mail->MsgHTML($htmlMessage);
+    $mail->MsgHTML($headers);
     $mail->AddAttachment('images/cardly.png');
     $mail->Send();
+
     echo "Mail sent";
 } catch (phpmailerException $e) {
     echo "<br><br> PHPMailer Exception => " . $e->errorMessage(); //Pretty error messages from PHPMailer
@@ -35,3 +72,5 @@ try {
     echo "<br><br> Generic Exception => " . $e->getMessage(); //Boring error messages from anything else!
 }
 echo "</pre>";
+header('Location: ../login.php');
+exit();
