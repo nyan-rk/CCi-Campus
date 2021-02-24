66065301227c8c9e123c7644b8f3b0684a3113ed
DarHashCCI <alss-dipsw20-hda@ccicampus.fr>
2021-02-24 19:38:12 +0100

    ... uh?!

--- diagprout.php
+++
@@ -1,7 +0,0 @@
-<?php  // On démarre la session AVANT toute chose
-    if (session_status() == PHP_SESSION_NONE) {
-        session_start();
-    }
-    include "./controller/initPage.php";
-    initPage();
-?>
